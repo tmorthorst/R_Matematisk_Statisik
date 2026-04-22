a3230be6020ea286463e416624543f58e2b83001
tmorthorst <tomo24ab@student.cbs.dk>
2026-04-22 21:30:39 +0200

    Lavet spg 4 i opg 55

--- uge_10.R
+++ uge_10.R
@@ -31,3 +31,25 @@ predict(regr_olympics_no_outliers, newdata = new_obs, interval = "confidence", l
 #fra delspm. 2? Forklar hvorfor.
 
 predict(regr_olympics_no_outliers, newdata = new_obs, interval = 'prediction', level = 0.95)
+
+
+#44. Find nu både 95% konfidensintervaller og 95% prædiktionintervaller for vægten
+#af atleter med højde p˚a hhv. 54, 60 og 66 inches. Hvad kan siges om den
+#relative størrelse af disse intervaller, n˚ar der varieres p˚a den forklarende variabel?
+#Forklar dette.
+
+obs_54 <- data.frame(Height = 54)
+obs_60 <- data.frame(Height = 60)
+obs_66 <- data.frame(Height = 66)
+
+konf_54 <- predict(regr_olympics_no_outliers, newdata = obs_54, interval = "confidence", level = 0.95)
+konf_60 <- predict(regr_olympics_no_outliers, newdata = obs_60, interval = "confidence", level = 0.95)
+konf_66 <- predict(regr_olympics_no_outliers, newdata = obs_66, interval = "confidence", level = 0.95)
+
+pred_54 <- predict(regr_olympics_no_outliers, newdata = obs_54, interval = "prediction", level = 0.95)
+pred_60 <- predict(regr_olympics_no_outliers, newdata = obs_60, interval = "prediction", level = 0.95)
+pred_66 <- predict(regr_olympics_no_outliers, newdata = obs_66, interval = "prediction", level = 0.95)
+
+data.frame(konf_54, pred_54)
+data.frame(konf_60, pred_60)
+data.frame(konf_66, pred_66)
